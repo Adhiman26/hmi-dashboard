9ec32445d275bf1734ef9591e2d650388504c60a
Aditya Manjrekar <adityamanjrekar@Adityas-MacBook-Air.local>
2026-02-02 14:10:12 +0530

    fixed the Netlify deployment issue

--- hmi-dashboard/src/App.tsx
+++ hmi-dashboard/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import './App.css';
 import Header from './components/Header';
 import RPMGauge from './components/RPMGauge';
